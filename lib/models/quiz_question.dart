e50217575bee962be8248afe66f4561da5b45489
EdgardYayo <allan.pazos@uabc.edu.mx>
2024-02-10 23:04:38 -0600

    Learning how to make models and how to render lists data inside Widgets with the map method and the spread operator

---
+++ lib/models/quiz_question.dart
@@ -0,0 +1,8 @@
+class QuizQuestion {
+  //constructor
+  const QuizQuestion(this.text, this.answers);
+  //properties
+  final String text;
+  final List<String> answers;
+
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,7 +1,9 @@
+import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/models/quiz_question.dart';
 import 'package:flutter/material.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
-
   const QuestionsScreen({super.key});
 
   @override
@@ -10,11 +12,31 @@ class QuestionsScreen extends StatefulWidget {
   }
 }
 
-
 class _QuestionScreenState extends State<QuestionsScreen> {
-
+  
   @override
   Widget build(context) {
-    return const Text('QuestionsScreen');
+
+    final QuizQuestion currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(color: Colors.white),
+          ),
+          const SizedBox(height: 30),
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(
+              answerText: answer, 
+              onTap: () {}
+            );
+          })
+        ],
+      ),
+    );
   }
 }
